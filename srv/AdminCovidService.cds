fd53e23a7584447999a0d6a07c74065b97aea328
Kai Niklas <kai.niklas@web.de>
2021-05-23 22:01:31 +0200

    refactor to use db service

--- srv/AdminCovidService.cds
+++
@@ -1,9 +0,0 @@
-namespace srv;
-
-using {db} from '../db/Covid';
-
-service AdminCovidService @(requires : 'authenticated-user') {
-    entity Countries             as projection on db.Countries;
-    entity CountryHistoryDetails as projection on db.CountryHistoryDetails;
-    entity Migrations            as projection on db.Migrations;
-}
--- srv/CovidService.js
+++ srv/CovidService.js
@@ -1,57 +1,48 @@
 const cds = require("@sap/cds");
 
 module.exports = cds.service.impl(async function () {
-  this.before("READ", "Countries", async (req, next) => {
-    const adminSrv = await cds.connect.to("srv.AdminCovidService");
-    const { Migrations, Countries } = adminSrv.entities;
+  const db = await cds.connect.to("db");
+  const { Migrations, Countries, CountryHistoryDetails } = db.entities;
 
+  this.before("READ", "Countries", async (req, next) => {
     // check if filled; then no need to execute
-    let migrations = await adminSrv.run(
-      SELECT.from(Migrations).where({
-        table: "Countries",
-        selector: "summary",
-      })
-    );
+
+    let migrations = await db.read(Migrations).where({
+      table: "Countries",
+      selector: "summary",
+    });
 
     if (migrations.length !== 0) {
       return;
     }
 
     // delete all countries
-    await adminSrv.run(DELETE.from(Countries));
+    await db.delete(Countries);
 
     // fetch daily summary from covid API
     const Covid19Api = await cds.connect.to("Covid19Api");
     var countries = await Covid19Api.run(req.query);
 
     // insert summary into Countries table
-    await adminSrv.run(INSERT.into(Countries).entries(countries));
+    await db.create(Countries).entries(countries);
 
-    // remember the data fetch
-    await adminSrv.run(
-      INSERT.into(Migrations).entries({
-        table: "Countries",
-        selector: "summary",
-        date: Date.now(),
-      })
-    );
+    await db.create(Migrations).entries({
+      table: "Countries",
+      selector: "summary",
+      date: Date.now(),
+    });
 
     return;
   });
 
   this.before("READ", "Countries/CountryHistoryDetails", async (req) => {
-    const adminSrv = await cds.connect.to("srv.AdminCovidService");
-    const { Migrations, CountryHistoryDetails } = adminSrv.entities;
-
     let country = req.params[0].Country;
 
     // check if filled; then no need to execute
-    let migrations = await adminSrv.run(
-      SELECT.from(Migrations).where({
-        table: "CountryHistoryDetails",
-        selector: country,
-      })
-    );
+    let migrations = await db.read(Migrations).where({
+      table: "CountryHistoryDetails",
+      selector: country,
+    });
 
     if (migrations.length !== 0) {
       return;
@@ -59,9 +50,7 @@ module.exports = cds.service.impl(async function () {
 
     // delete all history information
     try {
-      await adminSrv.run(
-        DELETE.from(CountryHistoryDetails).where({ Country: country })
-      );
+      await db.delete(CountryHistoryDetails).where({ Country: country });
     } catch (e) {}
 
     // fetch daily summary from covid API
@@ -69,18 +58,14 @@ module.exports = cds.service.impl(async function () {
     var countryHistoryDetails = await Covid19Api.run(req.query);
 
     // insert history entries from API
-    await adminSrv.run(
-      INSERT.into(CountryHistoryDetails).entries(countryHistoryDetails)
-    );
+    await db.create(CountryHistoryDetails).entries(countryHistoryDetails);
 
     // remember the data fetch
-    await adminSrv.run(
-      INSERT.into(Migrations).entries({
-        table: "CountryHistoryDetails",
-        selector: country,
-        date: Date.now(),
-      })
-    );
+    await db.create(Migrations).entries({
+      table: "CountryHistoryDetails",
+      selector: country,
+      date: Date.now(),
+    });
 
     return;
   });
